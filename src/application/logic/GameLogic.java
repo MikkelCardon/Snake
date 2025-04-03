9e429616f204ddb74b300302866b72979ba45f33
Mikkel Cardon <91466197+MikkelCardon@users.noreply.github.com>
2025-04-03 22:40:18 +0200

    generate food

--- src/application/logic/GameLogic.java
+++ src/application/logic/GameLogic.java
@@ -1,13 +1,39 @@
 package application.logic;
 
+import application.elements.Cell;
+import application.elements.snake.Snake;
+import controller.Controller;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
 
-public class GameLogic {
-    public void startGame() {
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(500), event -> {
+import java.util.Random;
 
-        }));
+public class GameLogic {
+
+    public GameLogic() {
+        new Snake();
+        generateFood();
+        startGame();
     }
+
+    public static void startGame() {
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(500), event -> {
+            KeyPressed.nextCell();
+            //UpdateVisuals.updateCells();
+        }));
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    public static void generateFood(){
+        Random rand = new Random();
+        Cell cell = Controller.returnCellByCoordinates(rand.nextInt(15), rand.nextInt(15));
+        while(cell.isSnake()){
+            cell = Controller.returnCellByCoordinates(rand.nextInt(15), rand.nextInt(15));
+        }
+        cell.setHasFood(true);
+    }
+
 }
